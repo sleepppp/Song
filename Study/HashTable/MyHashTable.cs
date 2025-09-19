de9dd2045d579d48d24e70be9c006cbc753544ca
sleepppp <32008212+sleepppp@users.noreply.github.com>
2025-09-19 20:13:08 +0900

    2025.09.19 수업자료

---
+++ Study/HashTable/MyHashTable.cs
@@ -0,0 +1,42 @@
+﻿public class MyHashTable
+{
+    public class Node
+    {
+        public int Value;
+        public string Key;
+        public Node NextNode;
+    }
+    public class Data
+    {
+        public string Key;
+        public int Value;
+        public Node NextNode;
+    }
+
+    private Data[] _arr = new Data[10];
+    private int _count = 0;
+
+    public void Add(string key, int value)
+    {
+        Data data = new Data();
+        data.Key = key;
+        data.Value = value;
+
+        int index = GetIndexFromKey(key);
+
+        if (_arr[index].Key != null)
+        {
+            //해싱 충돌!!! 여기부턴 연결 리스트로!
+        }
+        else
+        {
+            _arr[index] = data;
+        }
+        _count++;
+    }
+
+    private int GetIndexFromKey(string key)
+    {
+        return Math.Abs(key.GetHashCode()) % _arr.Length;
+    }
+}
---
+++ Study/HashTable/Program.cs
@@ -0,0 +1,3 @@
+﻿MyHashTable hashTable = new MyHashTable();
+hashTable.Add("권순우", 32);
+hashTable.Add("송재훈", 31);
